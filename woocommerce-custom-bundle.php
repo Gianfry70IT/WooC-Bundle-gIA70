67944b809ca4105879e763f170ba9eec05a0bdb0
Gianfranco Greco <gianfry70@gmail.com>
2025-09-14 20:26:55 +0200

    Aggiornamento versione a 0.9.2

--- woocommerce-custom-bundle.php
+++ woocommerce-custom-bundle.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name: WooC Bundle gIA70
  * Description: Un framework per creare prodotti bundle personalizzabili, unendo un'amministrazione stabile con un frontend funzionale.
- * Version: 0.9.1
+ * Version: 0.9.2
  * Author: gIA70 - Gianfranco Greco
  * Copyright (c) 2025 Gianfranco Greco
  * Licensed under the GNU GPL v2 or later: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: wcb-framework
  * GithubRepo: Gianfry70IT/WooC-Bundle-gIA70
- * Primary Branch:	main
+ * Primary Branch: main
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
