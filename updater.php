4900ebeafd8877422f2e8f4e1d70f694ddf3b7c2
Gianfranco Greco <gianfry70@gmail.com>
2025-09-14 19:35:13 +0200

    Aggiornamento versione a 0.8.6

--- updater.php
+++ updater.php
@@ -1,10 +1,12 @@
 <?php
-// file: updater.php
+/**
+ * WCB GitHub Updater (Versione Finale Definitiva)
+ * @version 1.4.0
+ */
 
 if ( ! class_exists( 'WCB_GitHub_Updater' ) ) {
     class WCB_GitHub_Updater {
         private $file;
-        private $plugin;
         private $basename;
         private $plugin_data;
         private $github_repo;
@@ -13,50 +15,80 @@ if ( ! class_exists( 'WCB_GitHub_Updater' ) ) {
         public function __construct( $file ) {
             $this->file = $file;
             add_action( 'admin_init', [ $this, 'set_plugin_properties' ] );
-            add_filter( 'pre_set_site_transient_update_plugins', [ $this, 'check_for_update' ] );
+            add_filter( 'pre_set_site_transient_update_plugins', [ $this, 'check_for_update' ], 10, 1 );
+            add_filter( 'upgrader_source_selection', [ $this, 'rename_github_zip' ], 10, 4 );
         }
 
         public function set_plugin_properties() {
-            $this->plugin      = get_plugin_data( $this->file );
             $this->basename    = plugin_basename( $this->file );
             $this->plugin_data = get_plugin_data( $this->file );
-            $this->github_repo = $this->plugin_data['GitHub Plugin URI'];
+            $this->github_repo = $this->get_github_repo_from_header();
+        }
+
+        private function get_github_repo_from_header() {
+            $plugin_file_content = file_get_contents( $this->file );
+            if ( preg_match( '/^[\s\*#@]*GithubRepo:(.*)$/mi', $plugin_file_content, $matches ) ) {
+                return trim( $matches[1] );
+            }
+            return '';
         }
 
         private function get_repo_release_info() {
-            if ( ! empty( $this->github_response ) || empty($this->github_repo) ) {
-                return;
-            }
-
+            if ( ! empty( $this->github_response ) || empty( $this->github_repo ) ) { return; }
             $url = "https://api.github.com/repos/{$this->github_repo}/releases/latest";
             $response = wp_remote_get( $url );
-
-            if ( is_wp_error( $response ) ) {
-                return false;
-            }
-
+            if ( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) { return; }
             $this->github_response = json_decode( wp_remote_retrieve_body( $response ) );
         }
 
         public function check_for_update( $transient ) {
-            if ( empty( $transient->checked ) ) {
+            if ( empty( $transient->checked ) || empty( $transient->checked[ $this->basename ] ) ) {
                 return $transient;
             }
 
             $this->get_repo_release_info();
             
-            if ( $this->github_response && version_compare( $this->github_response->tag_name, $transient->checked[ $this->basename ], '>' ) ) {
-                $package_url = $this->github_response->zipball_url;
+            if ( ! $this->github_response || ! isset( $this->github_response->tag_name ) ) {
+                return $transient;
+            }
+            
+            $installed_version = $transient->checked[ $this->basename ];
+            $github_version = trim( ltrim( $this->github_response->tag_name, 'vV' ) );
 
+            if ( version_compare( $github_version, $installed_version, '>' ) ) {
                 $obj = new stdClass();
-                $obj->slug = $this->basename;
+                $obj->slug        = $this->basename;
                 $obj->new_version = $this->github_response->tag_name;
-                $obj->url = $this->plugin["PluginURI"];
-                $obj->package = $package_url;
+                $obj->url         = $this->plugin_data["PluginURI"] ?? 'https://github.com/' . $this->github_repo;
+                $obj->package     = $this->github_response->zipball_url;
+                
+                if ( ! empty( $this->plugin_data['TestedUpto'] ) ) {
+                    $obj->tested = $this->plugin_data['TestedUpto'];
+                }
+                if ( ! empty( $this->plugin_data['RequiresWP'] ) ) {
+                    $obj->requires = $this->plugin_data['RequiresWP'];
+                }
+                
                 $transient->response[ $this->basename ] = $obj;
             }
 
             return $transient;
+        }
+
+        /**
+         * Rinomina la cartella del plugin estratta dallo zip di GitHub.
+         */
+        public function rename_github_zip( $source, $remote_source, $wp_upgrader, $hook_extra = null ) {
+            global $wp_filesystem;
+            
+            $plugin = $hook_extra['plugin'] ?? '';
+            if ( $plugin === $this->basename ) {
+                $new_source = trailingslashit( $remote_source ) . dirname( $this->basename );
+                $wp_filesystem->move( $source, $new_source, true );
+                return $new_source;
+            }
+
+            return $source;
         }
     }
 }
